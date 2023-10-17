49a12a3b0ede995fd783c3eb8e1bfacb17b1cc7a
Markham Lee <markhamlee@outlook.com>
2023-10-17 13:10:10 -0700

    linting

--- common/deviceTools.py
+++ common/deviceTools.py
@@ -1,45 +1,46 @@
-# Markham Lee (C) 2023 
-# Hardware Monitor for Linux & Windows: https://github.com/MarkhamLee/hardware-monitor
-#!/usr/bin/env python
-# simple script to generate unique IDs for each device, uses UUID4 as that ensures we get a truly
-# unique ID and not one derived from the MAC address, with the associated security risks such an 
-# approach can bring. 
-# Putting this into its own script (for now) as so the various clients can use it + I'll probably
-# put other utilities into this script, create an onboarding process, etc. 
+# !/usr/bin/env python
+# Markham Lee (C) 2023
+# Hardware Monitor for Linux & Windows:
+# https://github.com/MarkhamLee/hardware-monitor
+# simple script to generate unique IDs for each device, uses UUID4
+# as that ensures we get a truly unique ID and not one derived from
+# the MAC address, with the associated security risks such an
+# approach can bring.
+# Putting this into its own script (for now) as so the various
+# clients can use it + I'll probably put other utilities into this
+# script, create an onboarding process, etc.
 
 import uuid
-from paho.mqtt import client as mqtt 
-import time 
-import logging 
+from paho.mqtt import client as mqtt
+import logging
 import json
 import psutil
 
 
-# setup logging for static methods 
+# setup logging for static methods
 logging.basicConfig(filename='hardwareData.log', level=logging.DEBUG,
-format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+                    format='%(asctime)s %(levelname)s %(name)s %(threadName)s\
+                        : %(message)s')
 
 
 class DeviceUtilities():
 
-    # just a placeholder for now 
+    # just a placeholder for now
     def __init__(self):
 
         pass
 
-    
-    # method for parsing the config file with connection data + the secrets file 
+    # method for parsing the config file with connection data +
+    # the secrets file
     @staticmethod
     def loadConfigs(configFile, secretsFile):
 
- 
         with open(configFile, "r") as file:
             data = json.load(file)
 
         broker = data["broker"]
         port = data["port"]
         topic = data["topic"]
-
 
         with open(secretsFile, "r") as secrets:
             data = json.load(secrets)
@@ -49,14 +50,13 @@ class DeviceUtilities():
 
         return broker, port, topic, user, password
 
-
     @staticmethod
     def getClientID():
-        
+
         clientID = str(uuid.uuid4())
-        
+
         return clientID
-    
+
     @staticmethod
     def tempSensorScan():
 
@@ -67,30 +67,28 @@ class DeviceUtilities():
         sensorList = list(tempDict.keys())
 
         return sensorList
-    
 
     @staticmethod
     def mqttClient(clientID, username, pwd, host, port):
 
         def connectionStatus(client, userdata, flags, code):
-            
+
             if code == 0:
                 print('connected')
-              
+
             else:
                 print(f'connection error: {code} retrying...')
                 logging.DEBUG(f'connection error occured, return code: {code}')
 
-        
         client = mqtt.Client(clientID)
-        client.username_pw_set(username=username, password=pwd) 
-        client.on_connect=connectionStatus
+        client.username_pw_set(username=username, password=pwd)
+        client.on_connect = connectionStatus
 
         code = client.connect(host, port)
 
-        # this is so that the client will attempt to reconnect automatically/no need to add reconnect
-        # logic. 
+        # this is so that the client will attempt to reconnect automatically/
+        # no need to add reconnect
+        # logic.
         client.loop_start()
-        
 
-        return client, code 
+        return client, code
--- hardwareSensors/hardwareDataLinuxGPU.py
+++ hardwareSensors/hardwareDataLinuxGPU.py
@@ -1,55 +1,56 @@
 #!/usr/bin/env python
-# Markham Lee (C) 2023 
-# Hardware Monitor for Linux & Windows: https://github.com/MarkhamLee/hardware-monitor
-# this would be for linux devices where I'm not using an NVIDIA GPU/a GPU at all, e.g. 
-# single board computers running headless, or devices like an Intel NUC, so I would only
-# monitor the CPU 
+# Markham Lee (C) 2023
+# Hardware Monitor for Linux & Windows:
+# https://github.com/MarkhamLee/hardware-monitor
+# this would be for linux devices where I'm not using an NVIDIA GPU/a GPU
+# at all, e.g.single board computers running headless, or devices like an
+# Intel NUC so I would only monitor the CPU
 
 import json
-import time 
-from paho.mqtt import client as mqtt 
-import gc 
+import time
+import gc
 import os
 import logging
 import sys
 from linuxDataGPU import LinuxGPUSensors
 from linuxDataCPU import LinuxCpuData
 
-# this allows us to import modules, classes, scripts et al from the "common" directory 
+# this allows us to import modules, classes, scripts et al from the
+# "common" directory
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
 
-
 from common.deviceTools import DeviceUtilities
 
-
 logging.basicConfig(filename='hardwareDataLinuxGPU.log', level=logging.DEBUG,
-format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+                    format='%(asctime)s %(levelname)s %(name)s %(threadName)s\
+                        : %(message)s')
 
 
 def monitor(client, getData, getGpuData, topic):
 
-    while True: 
+    while True:
 
         time.sleep(1)
 
-        # get CPU utilization 
-        cpuUtil = getData.getCPUData() 
+        # get CPU utilization
+        cpuUtil = getData.getCPUData()
 
-        # get current RAM use 
+        # get current RAM use
         ramUse = getData.getRamData()
 
-        #get current freq and core count
+        # get current freq and core count
         cpuFreq, coreCount = getData.getFreq()
 
-        # get CPU temperature 
+        # get CPU temperature
         cpuTemp = getData.coreTemp()
 
-        # get GPU Data 
+        # get GPU Data
         temp, gpuLoad, gpuVram, gpuPower, gpuClock = getGpuData.gpuQuery()
 
-        # build payload 
-        payload = {"cpuTemp": cpuTemp,
+        # build payload
+        payload = {
+                   "cpuTemp": cpuTemp,
                    "cpuFreq": cpuFreq,
                    "cpuUse": cpuUtil,
                    "ramUse": ramUse,
@@ -64,49 +65,49 @@ def monitor(client, getData, getGpuData, topic):
 
         result = client.publish(topic, payload)
         status = result[0]
-                
         if status == 0:
             print(f'Data {payload} was published to: {topic}')
         else:
             print(f'Failed to send {payload} to: {topic}')
             logging.debug(f'MQTT publishing failure, return code: {status}')
 
-        del payload, cpuUtil, ramUse, cpuFreq, cpuTemp, status, result 
-        gc.collect() 
+        del payload, cpuUtil, ramUse, cpuFreq, cpuTemp, status, result
+        gc.collect()
 
 
-def main(): 
+def main():
 
-    # instantiate utilities class 
+    # instantiate utilities class
     deviceUtilities = DeviceUtilities()
 
-    # parse command line arguments 
+    # parse command line arguments
     args = sys.argv[1:]
 
     configFile = args[0]
     secrets = args[1]
 
     # load config file(s)
-    broker, port, topic, user, pwd = deviceUtilities.loadConfigs(configFile, secrets)
+    broker, port, topic, user, pwd = deviceUtilities.loadConfigs(configFile,
+                                                                 secrets)
 
-    # get unique client ID 
+    # get unique client ID
     clientID = deviceUtilities.getClientID()
 
-    # get mqtt client    
-    client, code = deviceUtilities.mqttClient(clientID, user, pwd, broker, port)
+    # get mqtt client
+    client, code = deviceUtilities.mqttClient(clientID, user, pwd, broker,
+                                              port)
 
-    # instantiate CPU data class & utilities class  
+    # instantiate CPU data class & utilities class
     getGpuData = LinuxGPUSensors()
-    getData = LinuxCpuData() 
+    getData = LinuxCpuData()
 
-
-    # start monitoring 
+    # start monitoring
     try:
         monitor(client, getData, getGpuData, topic)
 
     finally:
         client.loop_stop()
 
-    
+
 if __name__ == '__main__':
-      main() 
+    main()
