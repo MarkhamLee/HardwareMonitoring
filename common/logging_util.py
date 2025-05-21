e0a2c3c9269f7ee8ec18015b3c79f3f0946ce8a9
Markham Lee <markhamdev@markhamlee.com>
2025-05-20 22:48:44 -0700

    Updated to have separate methods for console vs log file logging

--- common/logging_util.py
+++ common/logging_util.py
@@ -1,13 +1,38 @@
-# Generic logging script
 import logging
+from logging.handlers import RotatingFileHandler
 from sys import stdout
 
 
-# set up/configure logging with stdout so it can be picked up by K8s
-logger = logging.getLogger('hardware_telemetry')
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(stdout)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(message)s')  # noqa: E501
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+def console_logging(name: str):
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.\
+        Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.propagate = False
+
+    return logger
+
+
+def log_file_logger(name):
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler(stdout)
+
+    handler = RotatingFileHandler(f'{name}_logfile.log', maxBytes=200000, backupCount=0)  # noqa: E501
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(message)s')  # noqa: E501
+    handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.addHandler(stream_handler)
+    logger.propagate = False
+
+    return logger
